2ebb38e842d2ce94a716389753a82d4e60fa131c
AIHelinfeng <97234740+AIHelinfeng@users.noreply.github.com>
2025-06-12 21:08:46 +0800

    feat: ai service and Mongodb

---
+++ src/main/java/org/pjlab/javaailangchain4j/assistant/MemoryChatAssistant.java
@@ -0,0 +1,19 @@
+package org.pjlab.javaailangchain4j.assistant;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
+/**
+ * 智能体
+ */
+@AiService(
+        wiringMode = EXPLICIT,
+        chatModel = "qwenChatModel",
+        chatMemory = "chatMemory"
+)
+public interface MemoryChatAssistant {
+}
---
+++ src/main/java/org/pjlab/javaailangchain4j/config/SeparateChatAssistantConfig.java
@@ -0,0 +1,27 @@
+package org.pjlab.javaailangchain4j.config;
+
+import org.pjlab.javaailangchain4j.store.MongoChatMemoryStore;
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeparateChatAssistantConfig {
+
+    @Autowired
+    private MongoChatMemoryStore mongoChatMemoryStore;
+    @Bean
+    public ChatMemoryProvider chatMemoryProvider(){
+
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .maxMessages(10)
+                //.chatMemoryStore(new InMemoryChatMemoryStore())
+                .chatMemoryStore(mongoChatMemoryStore)
+                .build();
+    }
+}
